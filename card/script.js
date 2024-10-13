103653b674b63617a4d51eeab754636940fc2f4d
Cesar <cesarwamartinez@gmail.com>
2024-10-12 21:26:10 -0500

    Card component

---
+++ card/script.js
@@ -0,0 +1,27 @@
+
+
+const contenedores = document.querySelectorAll(".container")
+
+
+console.log(contenedores)
+
+console.log(contenedores)
+contenedores.forEach(contenedor => {
+    contenedor.addEventListener("click",function(e){
+        e.stopPropagation()
+        if (e.target.style.height == "25vh"){
+            e.target.style.height = "fit-content"
+        }else if (e.target.style.height = "fit-content"){
+            e.target.style.height = "25vh"
+        }
+        console.log(e.target.style)
+    });
+
+    const childrens = document.querySelectorAll(".card-responsive")
+    console.log(childrens)
+    childrens.forEach(children => {
+        children.addEventListener("click",(e)=>{
+            e.stopPropagation()
+        })
+    })
+})
